5040d03ce65d1a39738aec13b18a1c67ad4dca56
amousavigourabi <28668597+amousavigourabi@users.noreply.github.com>
2026-01-05 23:22:50 +0100

    Pass event trigger details

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,29 @@
-type Handler<Result> = (params: PathParams) => Result;
+type Handler<Event, Result> = (pathParams: PathParams, event: Event) => Result;
+type ErrorHandler<Event, Result> = (event: Event) => Result;
 type NoSlash = string extends `${infer _Before}/${infer _After}` ? never : string;
 type Method = 'GET' | 'POST' | 'DELETE' | 'PUT' | 'PATCH';
-type PathParams = {
-  path: string;
-  [key: string]: string;
+type PathParams = Record<string, string>;
+type StaticChildren<Event, Result> = Record<NoSlash, RNode<Event, Result>>;
+type MethodHandlers<Event, Result> = Record<string, Handler<Event, Result>>;
+type Context<Event, Result> = {
+  error: ErrorHandler<Event, Result>,
 };
-type StaticChildren<Result> = {
-  [key: NoSlash]: RNode<Result>
-};
-type MethodHandlers<Result> = {
-  [key: string]: Handler<Result>
-};
-type Context<Result> = {
-  error: Handler<Result>
-}
 
-export class RNode<Result> {
-  private readonly staticChildren: StaticChildren<Result>;
+export class RNode<Event, Result> {
+  private readonly staticChildren: StaticChildren<Event, Result>;
   private paramChild?: {
-    node: RNode<Result>
+    node: RNode<Event, Result>
     param: string
   };
-  private readonly handlerByMethod: MethodHandlers<Result>;
+  private readonly handlerByMethod: MethodHandlers<Event, Result>;
 
   constructor() {
     this.handlerByMethod = {};
     this.staticChildren = {};
   }
 
-  on(path: NoSlash): RNode<Result> {
-    const node = new RNode<Result>();
+  on(path: NoSlash): RNode<Event, Result> {
+    const node = new RNode<Event, Result>();
     if (path[0] === ':') {
       const param = path.slice(1);
       this.paramChild = {
@@ -42,36 +36,36 @@ export class RNode<Result> {
     return node;
   }
 
-  method(method: Method, handler: Handler<Result>): void {
+  method(method: Method, handler: Handler<Event, Result>): void {
     this.handlerByMethod[method] = handler;
   }
 
-  route(path: Array<NoSlash>, position: number, params: PathParams, ctx: Context<Result>): Result {
+  route(path: Array<NoSlash>, position: number, params: PathParams, event: Event, ctx: Context<Event, Result>): Result {
     const segment = path[position];
     if (path.length === position - 1) {
-      return this.handlerByMethod[segment](params) ?? ctx.error(params);
+      return this.handlerByMethod[segment](params, event) ?? ctx.error(event);
     } else if (this.paramChild) {
       params[this.paramChild.param] = segment;
-      return this.paramChild.node.route(path, position + 1, params, ctx);
+      return this.paramChild.node.route(path, position + 1, params, event, ctx);
     } else {
-      return this.staticChildren[segment]?.route(path, position + 1, params, ctx) ?? ctx.error(params);
+      return this.staticChildren[segment]?.route(path, position + 1, params, event, ctx) ?? ctx.error(event);
     }
   }
 }
 
-export class Router<Result> {
-  private readonly errorHandler: Handler<Result>;
-  private readonly root: RNode<Result>;
+export class Router<Event, Result> {
+  private readonly errorHandler: ErrorHandler<Event, Result>;
+  private readonly root: RNode<Event, Result>;
 
-  constructor(err: Handler<Result>) {
+  constructor(err: ErrorHandler<Event, Result>) {
     this.errorHandler = err;
-    this.root = new RNode<Result>();
+    this.root = new RNode<Event, Result>();
   }
 
-  route(path: string, method: Method) {
+  route(path: string, method: Method, event: Event) {
     const segments = path.split('/');
     segments.push(method);
-    return this.root.route(segments, 0, { path }, { error: this.errorHandler });
+    return this.root.route(segments, 0, {}, event, { error: this.errorHandler });
   }
 
   buildApi() {
@@ -79,6 +73,6 @@ export class Router<Result> {
   }
 }
 
-export function registerRouter<Result>(err: Handler<Result>) {
-  return new Router<Result>(err);
+export function registerRouter<Event, Result>(err: ErrorHandler<Event, Result>) {
+  return new Router<Event, Result>(err);
 }
